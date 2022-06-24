32d9804939bd38b4fe14ca714f036638c21e9a88
hemant gangad <hemantgangad3@gmail.com>
2022-06-24 13:45:35 +0530

    class Assignment

---
+++ SimpleInterest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class SimpleInterest
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		float p,r,t,temp=1;
+		float si=0.0f,sum=0f;
+		System.out.print("Enter Principle Amount: ");
+		p=sc.nextFloat();
+		System.out.print("Enter rate            : ");
+		r=sc.nextFloat();
+		System.out.print("Enter Time            : ");
+		t=sc.nextFloat();
+		System.out.println("\tYear\tPrinciple Amount\tRate\tSimpleInterest");
+		while(temp<=t)
+		{
+			si=(p*r*temp)/100;
+			sum=sum+si;
+			System.out.println("\t"+temp+"\t       "+p+"\t\t "+r+"\t    "+si);
+			temp++;
+		}
+			System.out.println("\t\t\t\t\t------------------------");
+			System.out.println("\t\t\t\t\tTotal Pay = "+(sum+p));
+			
+	}
+}
